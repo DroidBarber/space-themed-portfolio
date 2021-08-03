68b164fcebe2bb15c1f2d7fea606b3977472e81c
DroidBarber <60230244+DroidBarber@users.noreply.github.com>
2021-08-03 16:05:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from '/three';
+import * as THREE from "../node_modules/three/build/three.module.js
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
